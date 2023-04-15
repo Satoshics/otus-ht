a4592f63426126911cb1ed47c3b86451ab9b3d14
₿ Satoshic <114339498+Anonyc0@users.noreply.github.com>
2023-04-15 16:25:31 +0500

    81

---
+++ hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include "move_me_out.h"
+int main()
+{
+    somefunc();
+}
---
+++ move_me_out.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using std::string;
+void somefunc()
+{
+    string line = "Hello world!\n";
+    std::cout << line;
+}
+
